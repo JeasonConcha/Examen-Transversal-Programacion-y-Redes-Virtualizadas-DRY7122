b721f16323af4173a7d2d71d4e83caa7702fe873
root <root@labvm.vm>
2023-07-10 21:25:48 +0000

    Script de usuarios y claves

--- examen.py
+++ examen.py
@@ -1,41 +1,55 @@
 # -*-coding: utf-8 -*-
-from flask import flask, request, jsonify
+ffrom flask import Flask, request, jsonify
 from passlib.hash import sha256_crypt
 import sqlite3
 
-@app.route('/register' , methods=['POST'])
-def register_user ():
-data = requests.get_json()
-username = data ['username']
-password = data['password']
+app = Flask(name)
 
-hashed_password = sha256_crypt.hash(password)
+@app.route('/register', methods=['POST'])
+def register_user():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
 
-with sqlite3.connect('ET.db') as conn:
-curson = conn.cursor()
-cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_password))
-conn.commit()
-return jsonify({"message": "Usuario registrado con exito."}),201
--
-@app.route('/login' , methods=['POST'])
-def login_user ():
-data = request.get_json()
-username = data['username']
-password = data['password']
+    hashed_password = sha256_crypt.hash(password)
 
-with sqlite3.connect('ET.db') as conn:
-curson = conn.cursor()
-cursor.execute("SELECT password FROM users WHERE username=?, (username,))
-result = cursor.fetchone()
+    with sqlite3.connect('ET.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_password))
+        conn.commit()
 
-if result and sha256_crypt.verify(password, result[0]):
-return jsonify({"message": "Inicio de sesion exitoso."}),200
-else:
-return jsonify({"message": "Nombre de usuario o contraseña incorrectos."}), 401
+    return jsonify({"message": "Usuario registrado con éxito."}), 201
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+
+    with sqlite3.connect('ET.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT password FROM users WHERE username=?", (username,))
+        result = cursor.fetchone()
+
+        if result and sha256_crypt.verify(password, result[0]):
+            return jsonify({"message": "Inicio de sesión exitoso."}), 200
+        else:
+            return jsonify({"message": "Nombre de usuario o contraseña incorrectos."}), 401
 
 @app.route('/')
 def home():
-return "!Bienvenido al sitio web de gestion de claves!"
+    return "!Bienvenido al sitio web de gestión de claves!"
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Página no encontrada", 404
+
+if name == 'main':
+    with sqlite3.connect('ET.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)")
+        conn.commit()
+    app.run(host='0.0.0.0', port=9500)
 
 @app.errorhandler(404)
 def page_not_found(e):
