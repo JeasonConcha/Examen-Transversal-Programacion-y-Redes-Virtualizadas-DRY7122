fc14114bebba262494739ed746daa9704f653830
root <root@labvm.vm>
2023-07-10 19:25:42 +0000

    Script de calculo de distancia

--- CalcularDistancia.py
+++ CalcularDistancia.py
@@ -21,6 +21,6 @@ def calcular_distancia(ciudad_origen, ciudad_destino):
 
 distancia_km = calcular_distancia(ciudad_origen, ciudad_destino)
 if distancia_km:
-    print(f"La distancia entre {ciudad_origen} y {ciudad_destino} es de {distancia_km:.1f} km.")
+    print("La distancia entre {ciudad_origen} y {ciudad_destino} es de {distancia_km:.1f} km.")
 else:
     print("No se pudo calcular la distancia.")
