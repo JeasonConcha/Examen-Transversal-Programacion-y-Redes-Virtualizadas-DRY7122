fa4eaf1140cc628ae2e020c1753fdf49c644405e
root <root@labvm.vm>
2023-07-10 21:13:28 +0000

    Script de usuarios y claves

---
+++ examen.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, jsonify
+from passlib.hash import sha256_crypt
+import sqlite3
+
+app = Flask(name)
+
+@app.route('/register', methods=['POST'])
+def register_user():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+
+    hashed_password = sha256_crypt.hash(password)
+
+    with sqlite3.connect('ET.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_password))
+        conn.commit()
+
+    return jsonify({"message": "Usuario registrado con éxito."}), 201
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+
+    with sqlite3.connect('ET.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT password FROM users WHERE username=?", (username,))
+        result = cursor.fetchone()
+
+        if result and sha256_crypt.verify(password, result[0]):
+            return jsonify({"message": "Inicio de sesión exitoso."}), 200
+        else:
+            return jsonify({"message": "Nombre de usuario o contraseña incorrectos."}), 401
+
+@app.route('/')
+def home():
+    return "!Bienvenido al sitio web de gestión de claves!"
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "Página no encontrada", 404
+
+if name == 'main':
+    app.run(port=9500)
+
