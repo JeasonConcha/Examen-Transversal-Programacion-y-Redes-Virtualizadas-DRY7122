999099bb5f92dc7f75b4bcb36878b70e6951c86d
root <root@labvm.vm>
2023-07-10 19:58:33 +0000

    Script de calculo de distancia

--- CalcularDistancia.py
+++ CalcularDistancia.py
@@ -1,26 +1,27 @@
-from geopy.geocoders import Nominatim
-from geopy.distance import geodesic
+import requests
+from geopy.geocoders import nominatim
+from geopy import distance
 
-def obtener_coordenadas(ciudad):
-    geolocator = Nominatim(user_agent="my_app")
-    location = geolocator.geocode(ciudad)
-    if location:
-        return location.latitude, location.longitude
-    else:
-        return None
+def obtener_distancia(cuidad_origin cuidad_destino):
+	geolocator = Nominatim(user_agent="app")
+	location_origen = geolocator.geocode(ciudad_origin)
+	location_destino= geolocator.geocode(ciudad_destino)
+	coords_origen = (location_origen.latitude, location_origen.longitude)
+	coords_destino = (location_destino.latitude, location_destino.longitude)
+	return distance.distance(coords_origen, coords_destino).km
+ciudad_origen = input ("Ciudad De Origen: ")
+ciudad_destino = input("Ciudad De Destino: ")
 
-def calcular_distancia(ciudad_origen, ciudad_destino):
-    coordenadas_origen = obtener_coordenadas(ciudad_origen)
-    coordenadas_destino = obtener_coordenadas(ciudad_destino)
+distancia_km = obtener_distancia(ciudad_origen, ciudad_destino)
 
-    if coordenadas_origen and coordenadas_destino:
-        distancia = geodesic(coordenadas_origen, coordenadas_destino).kilometers
-        return distancia
-    else:
-        return None
+tiempo_horas =distancia / 100
+tiempo_minutos = tiempo_horas * 60
+tiempo_segundos = tiempo_minutos * 60
 
-distancia_km = calcular_distancia(ciudad_origen, ciudad_destino)
-if distancia_km:
-    print("La distancia entre {ciudad_origen} y {ciudad_destino} es de {distancia_km:.1f} km.")
-else:
-    print("No se pudo calcular la distancia.")
+combustible_litros = distancia_km * 0.15
+
+print("Duracion del viaje: {} horas, {} minutos, {} segundos" .format(int(tiempo_horas), int(tiempo_minutos), int(tiempo,segundos)))
+print("Combustible requerido: litros" .format(round(combustible_litros, 1)))
+
+print("¡Estas a punto de viajar desde {} a {}!" .format(cuidad_origen, ciudad_destino))
+print("El viaje tiene una distancia de {} km y tomara aproximadamente {} horas." .format(round(distancia_km, 1) int(tiempo_horas)))
