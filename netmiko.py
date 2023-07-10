3230238e4d2e962abe7f7cf097662d24f1db8d60
root <root@labvm.vm>
2023-07-10 22:56:29 +0000

    netmiko

---
+++ netmiko.py
@@ -0,0 +1,18 @@
+import netmiko  
+import json
+from netmiko import connecthandler
+
+cisco1 = {
+    "ip": "10.0.2.15",
+    "device_type": "cisco_ios",
+    "username": "netmiko",
+    "password":"cisco123!"
+}
+# show command that we execute.
+command = "show run"
+with connecthandler(**cisco1) as net_connect:
+    output = net_connect.send_command(command)
+    #autmaticallly cleans up the output so that show output is returned
+    print()
+    print(output)
+    print()
